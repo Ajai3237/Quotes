e277489ecdba47d647a8327b7b7a740f9a66f145
ajai krishna m <ajai12346aji@gmail.com>
2025-03-03 18:51:56 +0530

    red

--- component/Box.jsx
+++ component/Box.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../component/Box.css";
 import axios from "axios";
-// import request from "../src/Request";
+import request from "../src/Request";
 
 function Box() {
     const API = "https://dummyjson.com/quotes";
@@ -25,24 +25,7 @@ function Box() {
         fetchQuote();
     }, []);
 
-    const request = {
-        Rumi: 'src/assets/img/download.jpeg',
-        AbdulKalam:'src/assets/img/download (1).jpeg',
-        BillGates:'src/assets/img/download (2).jpeg',
-        AbuBakr:'src/assets/img/download(3).jpeg',
-        AlbertEinstein:'src/assets/img/download (4).jpeg',
-        AbrahamLincoln:'src/assets/img/download (5).jpeg',
-        OprahWinfrey:'src/assets/img/download (6).jpeg',
-        MuhammadAli:'src/assets/img/ali.jpeg',
-        MotherTeresa:'src/assets/img/download (8).jpeg',
-        NelsonMandela:'src/assets/img/download (9).jpeg',
-        UmaribnAl:'src/assets/img/download (10).jpeg',
-        WaltDisney:'src/assets/img/download (11).jpeg',
-        Aristotle:'src/assets/img/download (12).jpeg',
-    
-    }
-
-
+   
 
 
     const handleNextClick = () => {
