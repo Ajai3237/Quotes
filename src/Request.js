df6c3cfde8e336de29d6fb2e091c91bfaa9ac395
ajai krishna m <ajai12346aji@gmail.com>
2025-03-03 18:48:54 +0530

    d

--- src/Request.js
+++ src/Request.js
@@ -1,17 +1,17 @@
-const request = {
-    Rumi: 'src/assets/img/download.jpeg',
-    AbdulKalam:'src/assets/img/download (1).jpeg',
-    BillGates:'src/assets/img/download (2).jpeg',
-    AbuBakr:'src/assets/img/download(3).jpeg',
-    AlbertEinstein:'src/assets/img/download (4).jpeg',
-    AbrahamLincoln:'src/assets/img/download (5).jpeg',
-    OprahWinfrey:'src/assets/img/download (6).jpeg',
-    MuhammadAli:'src/assets/img/ali.jpeg',
-    MotherTeresa:'src/assets/img/download (8).jpeg',
-    NelsonMandela:'src/assets/img/download (9).jpeg',
-    UmaribnAl:'src/assets/img/download (10).jpeg',
-    WaltDisney:'src/assets/img/download (11).jpeg',
-    Aristotle:'src/assets/img/download (12).jpeg',
+// const request = {
+//     Rumi: 'src/assets/img/download.jpeg',
+//     AbdulKalam:'src/assets/img/download (1).jpeg',
+//     BillGates:'src/assets/img/download (2).jpeg',
+//     AbuBakr:'src/assets/img/download(3).jpeg',
+//     AlbertEinstein:'src/assets/img/download (4).jpeg',
+//     AbrahamLincoln:'src/assets/img/download (5).jpeg',
+//     OprahWinfrey:'src/assets/img/download (6).jpeg',
+//     MuhammadAli:'src/assets/img/ali.jpeg',
+//     MotherTeresa:'src/assets/img/download (8).jpeg',
+//     NelsonMandela:'src/assets/img/download (9).jpeg',
+//     UmaribnAl:'src/assets/img/download (10).jpeg',
+//     WaltDisney:'src/assets/img/download (11).jpeg',
+//     Aristotle:'src/assets/img/download (12).jpeg',
 
-}
-export default request
+// }
+// export default request
